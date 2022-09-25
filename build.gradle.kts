ed9f47575826df42f00fea661789a318d1ac81fe
DQMME <dqmme1@gmail.com>
2022-09-25 18:33:48 +0200

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.Locale
 
 plugins {
-    id("com.google.devtools.ksp") version "1.7.0-1.0.6"
-    kotlin("jvm") version "1.7.0"
-    kotlin("plugin.serialization") version "1.7.0"
-    id("dev.schlaubi.mikbot.gradle-plugin") version "2.4.1"
+    id("com.google.devtools.ksp") version "1.7.10-1.0.6"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
+    id("dev.schlaubi.mikbot.gradle-plugin") version "2.6.2"
 }
 
 group = "de.dqmme"
@@ -20,12 +20,12 @@ repositories {
 dependencies {
     @Suppress("DependencyOnStdlib")
     compileOnly(kotlin("stdlib-jdk8"))
-    mikbot("dev.schlaubi", "mikbot-api", "3.3.0-SNAPSHOT")
+    mikbot("dev.schlaubi", "mikbot-api", "3.14.0-SNAPSHOT")
     ksp("dev.schlaubi", "mikbot-plugin-processor", "2.2.0")
     implementation("com.github.twitch4j", "twitch4j", "1.10.0")
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-rx2", "1.6.2")
     implementation("com.github.akarnokd", "rxjava2-interop", "0.13.7")
-    plugin("dev.schlaubi", "mikbot-game-animator", "2.4.0")
+    plugin("dev.schlaubi", "mikbot-game-animator", "2.8.0")
 }
 
 mikbotPlugin {
